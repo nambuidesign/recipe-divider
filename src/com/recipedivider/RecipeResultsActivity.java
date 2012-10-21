8b8a7ce065f299fecbc629e27f9892af75bcdc25
Matthew Michihara <matthew.michihara@gmail.com>
2012-10-21 15:32:35 -0700

    Stop tracking generated files

---
+++ src/com/recipedivider/RecipeResultsActivity.java
@@ -0,0 +1,13 @@
+package com.recipedivider;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecipeResultsActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_recipe_results);
+
+	}
+}
